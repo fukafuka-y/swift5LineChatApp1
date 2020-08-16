5829bf04ab5b65c219586d1c9271a0a899c17c6c
fukayayuto <fukaya1287@gmail.com>
2020-08-16 14:49:09 +0900

    一時保存

--- swift5LineChatApp1/Controller/SingUpViewController.swift
+++ swift5LineChatApp1/Controller/SingUpViewController.swift
@@ -27,18 +27,75 @@ class SingUpViewController:UIViewController{
         userImageView.layer.borderWidth = 1
         userImageView.layer.backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240).cgColor
         registerButton.layer.cornerRadius = 15
+        registerButton.isEnabled = false
+        registerButton.backgroundColor = UIColor.rgb(red: 100, green: 100, blue: 100)
         
+        userImageView.addTarget(self, action: #selector(tapImageView), for: .touchUpInside)
+        
+        mailTextField.delegate = self
+        passwordTextField.delegate = self
+        usernameTextField.delegate = self
+    }
+  
+    @objc private func tapImageView(){
+        print("tapImage")
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        self.present(imagePickerController, animated: true, completion: nil)
+    
     }
     
     
+    
     @IBAction func register(_ sender: Any) {
-        
+        print("register")
     }
     
     
     @IBAction func userTapped(_ sender: Any) {
+        print("tapUser")
+    }
+    
+}
+
+extension SingUpViewController:UITextFieldDelegate{
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+
+        let mailIsEmpty = mailTextField.text?.isEmpty ?? false
+        let passwordISEmpty = passwordTextField.text?.isEmpty ?? false
+        let usernameIsEmpty = usernameTextField.text?.isEmpty ?? false
+        
+        if mailIsEmpty || passwordISEmpty || usernameIsEmpty{
+            registerButton.isEnabled = false
+            registerButton.backgroundColor = UIColor.rgb(red: 100, green: 100, blue: 100)
+        }else{
+            registerButton.isEnabled = true
+            registerButton.backgroundColor = UIColor.rgb(red: 0, green: 180, blue: 0)
+        }
+        
+        
         
     }
     
 }
 
+extension SingUpViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editImage = info[.editedImage] as? UIImage {
+            userImageView.setImage(editImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }else if let originalImage = info[.originalImage] as? UIImage{
+            userImageView.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+        userImageView.setTitle("", for: .normal)
+        userImageView.imageView?.contentMode = .scaleToFill
+        userImageView.contentHorizontalAlignment = .fill
+        userImageView.contentVerticalAlignment = .fill
+        userImageView.clipsToBounds = true
+        
+        dismiss(animated: true, completion: nil)
+ }
+}
